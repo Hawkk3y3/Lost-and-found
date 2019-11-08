d7ac87cfbb7fb4bb1eb365cd151e75e0ad3f0d2a
hawkeye <usmanmani781@gmail.com>
2019-11-08 12:38:02 +0500

    Made Changes in the Code to follow the pip8 standards

--- app/APIStart.py
+++ app/APIStart.py
@@ -9,147 +9,152 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 import json
-from DBhandler import DatabaseHandler as db
-from Items import Item as item
-from User import User
+from app.DBhandler import DatabaseHandler
+from app.Items import Item
+from app.User import User
 
 app = Flask(__name__)
-
-
 
 
 @app.route('/')
 def index():
     return jsonify('This is index page')
 
-@app.route('/items/create',methods=['POST'])
+
+@app.route('/items/create', methods=['POST'])
 def create():
     try:
-        testdb = db()
+        database_handler = DatabaseHandler()
         name = request.json['name']
         loc = request.json['location']
         desc = request.json['description']
-        items = item()
-        items.newItem(name,loc,desc)
-        testdb.insert_item_db(items)
-        
-        resp = jsonify({"Action":'Item Added Successfully'})
+        items = Item()
+        items.new_item(name, loc, desc)
+        database_handler.insert_item_db(items)
+
+        resp = jsonify({"Action": 'Item Added Successfully'})
         resp.status_code = 200
         return resp
-        
+
     except Exception as e:
         print(e)
-        
-@app.route('/items/update',methods=['POST'])
+
+
+@app.route('/items/update', methods=['POST'])
 def update():
     try:
-        testdb = db()
-        item_id =request.json['item_id'] 
+        database_handler = DatabaseHandler()
+        item_id = request.json['item_id']
         name = request.json['name']
         loc = request.json['location']
         desc = request.json['description']
-        items = item()
-        items.newItem(name,loc,desc)
-        result = testdb.update_item(items,item_id)
+        items = Item()
+        items.new_item(name, loc, desc)
+        result = database_handler.update_item(items, item_id)
         if result:
-            resp = jsonify({"Action":'Item Updated Successfully'})
+            resp = jsonify({"Action": 'Item Updated Successfully'})
             resp.status_code = 200
             return resp
         else:
-            resp = jsonify({"Action":'Item with Item Id {} is not found'.format(item_id)})
+            resp = jsonify({"Action": 'Item with Item Id {} is not found'.format(item_id)})
             resp.status_code = 200
             return resp
-        
+
     except Exception as e:
         print(e)
-@app.route('/items/view',methods=['GET'])
+
+
+@app.route('/items/view', methods=['GET'])
 def view():
-    testdb = db()
-    resultlist=testdb.view_items()
-    json_string = json.dumps([ob.__dict__ for ob in resultlist])
+    database_handler = DatabaseHandler()
+    result_list = database_handler.view_items()
+    json_string = json.dumps([ob.__dict__ for ob in result_list])
     return json_string
-@app.route('/items/delete/<int:item_id>',methods=['DELETE'])
+
+
+@app.route('/items/delete/<int:item_id>', methods=['DELETE'])
 def delete_item(item_id):
     try:
-        testdb = db()
-        result=testdb.delete_item(item_id)
+        database_handler = DatabaseHandler()
+        result = database_handler.delete_item(item_id)
         if result:
-            resp = jsonify({"Action":'Item Deleted Successfully {}'.format(item_id)})
+            resp = jsonify({"Action": 'Item Deleted Successfully {}'.format(item_id)})
             resp.status_code = 200
             return resp
         else:
-            resp = jsonify({"Action":'Item Not Found with id {}'.format(item_id)})
+            resp = jsonify({"Action": 'Item Not Found with id {}'.format(item_id)})
             resp.status_code = 200
             return resp
     except Exception as e:
         print(e)
-@app.route('/item/search/<string:loc>',methods=['GET'])
+
+
+@app.route('/item/search/<string:loc>', methods=['GET'])
 def search_item(loc):
     try:
-        testdb = db()
-        resultlist = testdb.search_item_by_loc(loc)
-        json_string = json.dumps([ob.__dict__ for ob in resultlist])
+        database_handler = DatabaseHandler()
+        result_list = database_handler.search_item_by_loc(loc)
+        json_string = json.dumps([ob.__dict__ for ob in result_list])
         return json_string
     except Exception as e:
         print(e)
 
-@app.route('/item/search/<string:name>',methods=['PUT'])
+
+@app.route('/item/search/<string:name>', methods=['PUT'])
 def search_item_name(name):
     try:
-        testdb = db()
-        resultlist = testdb.search_item_by_name(name)
-        json_string = json.dumps([ob.__dict__ for ob in resultlist])
+        database_handler = DatabaseHandler()
+        result_list = database_handler.search_item_by_name(name)
+        json_string = json.dumps([ob.__dict__ for ob in result_list])
         return json_string
     except Exception as e:
         print(e)
-    
-@app.route('/user/register',methods=['POST'])
+
+
+@app.route('/user/register', methods=['POST'])
 def register_user():
     try:
-        testdb = db()
-        username =request.json['username'] 
+        database_handler = DatabaseHandler()
+        username = request.json['username']
         email = request.json['email']
         password = request.json['password']
         user = User()
-        user.User(username,email,password)
-        
-        excep=testdb.register_user(user)
-        
-        if excep:
-            resp = jsonify({"Action":'Error occured {}'.format(excep)})
+        user.user(username, email, password)
+
+        response = database_handler.register_user(user)
+
+        if response:
+            resp = jsonify({"Action": 'Error occured {}'.format(response)})
             resp.status_code = 200
             return resp
         else:
-            resp = jsonify({"Action":'User Registered Successfully'})
+            resp = jsonify({"Action": 'User Registered Successfully'})
             resp.status_code = 200
-            return resp        
-        
-        
+            return resp
+
     except Exception as e:
         print(e)
 
 
-@app.route('/user/login',methods=['POST'])
+@app.route('/user/login', methods=['POST'])
 def login_user():
     try:
-        testdb = db() 
+        database_handler = DatabaseHandler()
         email = request.json['email']
         password = request.json['password']
-        
-        if testdb.login_user(email,password):
-            resp = jsonify({"Action":'User Logged In Successfully'})
+
+        if database_handler.login_user(email, password):
+            resp = jsonify({"Action": 'User Logged In Successfully'})
             resp.status_code = 200
             return resp
         else:
-            resp = jsonify({"Action":'User Login Failure',"reason":"Email or Password Incorrect"})
+            resp = jsonify({"Action": 'User Login Failure', "reason": "Email or Password Incorrect"})
             resp.status_code = 200
             return resp
-        
-        
+
     except Exception as e:
         print(e)
-
 
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
