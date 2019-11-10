1d537b1789b3fee4c94d2d878d485c348c183039
Hawkk3y3 <usmanmani781@gmail.com>
2019-11-10 23:46:58 +0500

    applied pip8 standard to the code

--- app/playground.py
+++ app/playground.py
@@ -5,15 +5,15 @@ Created on Thu Oct 31 16:38:39 2019
 @author: Hawkeye
 """
 
-from DBhandler import DatabaseHandler as db
-from User import User
-from Items import Item as item
+from app.DBhandler import DatabaseHandler
+from app.User import User
+from app.Items import Item
 import time
 
 
 time.sleep(5)
 
-testdb = db()
+test_db = DatabaseHandler()
 # user = User()
 # user.username="hawkeye"
 # user.email="hawkeye@gmail.com"
@@ -23,7 +23,7 @@ testdb = db()
 # name = 'name'
 # loc = 'location'
 # desc = 'description'
-testdb.delete_item(1)
+test_db.delete_item(1)
 
 # items.newItem(name,loc,desc)
 # testdb.insert_item_db(items)
